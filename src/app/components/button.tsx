9d23c0c541c57188e7fb3d2c9353bd6b54c23e0a
alberterc <alberterc.ps@gmail.com>
2024-07-02 20:07:21 +0700

    feat: add website embed capability
    - moved /components into /app/components
    - add onClick handler to button component

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,8 +1,8 @@
-const Button = ({text, className} : {text: string, className?: string}) => {
+const Button = ({text, className, onClick} : {text: string, className?: string, onClick?: () => void}) => {
     const defaultClassName = "rounded-full border-2 border-white bg-inherit text-inherit px-7 py-1 transition-colors focus:outline-none hover:bg-neutral-800 active:bg-white active:text-black";
     const finalClassName = className ? `${defaultClassName} ${className}` : defaultClassName;
     return (
-        <button className={finalClassName}>
+        <button className={finalClassName} onClick={onClick}>
             {text}
         </button>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
-import Button from "@/components/button";
-import MainResults from "@/components/mainResults";
+'use client';
+
+import Button from "@/app/components/button";
+import InsertEmbedButton from "@/app/components/insertEmbedButton";
+import MainResults from "@/app/components/mainResults";
+import { useState } from "react";
 
 export default function Home() {
+  const [embedUrl, setEmbedUrl] = useState('');
+  const resetEmbedBox = () => {
+    setEmbedUrl('');
+    const embedBox = document.getElementById('embedBox')!;
+    if (embedBox.getElementsByTagName('ul')[0].classList.contains('hidden')) {
+      embedBox.getElementsByTagName('ul')[0].classList.toggle('hidden');
+      embedBox.getElementsByTagName('iframe')[0].classList.toggle('hidden');
+    }
+  };
+  const getEmbedUrl = (str: string) => {
+    let res = str.trim();
+    setEmbedUrl(res);
+    const embedBox = document.getElementById('embedBox')!;
+
+    if (res === '') {
+      resetEmbedBox();
+    }
+    else if (!embedBox.getElementsByTagName('ul')[0].classList.contains('hidden')) {
+      embedBox.getElementsByTagName('ul')[0].classList.toggle('hidden');
+      embedBox.getElementsByTagName('iframe')[0].classList.toggle('hidden');
+    }
+  };
+
   return (
     <main className="text-white grow flex flex-row items-center gap-10">
       <div id="mainContainer" className="pl-24 grow">
@@ -19,18 +46,22 @@ export default function Home() {
         </div>
       </div>
       <div id="embedBoxContainer" className="self-stretch w-2/5">
-        <div id="embedBox" className="flex justify-center items-center rounded-lg px-4 py-2 bg-neutral-800 h-full">
+        <div id="embedBox" className="flex flex-col justify-center items-center rounded-lg px-4 py-2 bg-neutral-800 h-full">
           <ul className="list-disc list-inside leading-10">
             <li>Add a website of your choosing to embed <a href="">here</a>.</li>
             <li>One common use is for <a href="https://jisho.org/">jisho.org</a>.</li>
             <li>There are some websites which limits this function which I cannot do anything about.</li>
           </ul>
-          {/* <iframe 
-            src="https://jisho.org/"
+          <iframe
+            src={embedUrl}
             width='100%'
             height='100%'
-            className='border-0 p-0'
-          ></iframe> */}
+            className='hidden border-0 p-0'
+          ></iframe>
+          <div className="flex flex-row gap-3 mt-2">
+            <InsertEmbedButton embedData={getEmbedUrl}/>
+            <Button text='Reset Embed' onClick={() => resetEmbedBox()}/>
+          </div>
         </div>
       </div>
     </main>
